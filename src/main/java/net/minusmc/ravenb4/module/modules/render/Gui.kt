3222c487f9e7c854d5636983858ca7b3f16220fa
Panagiotis3149 <138581609+Panagiotis3149@users.noreply.github.com>
2024-06-04 11:27:41 +0000

    Update Gui.kt

--- src/main/java/net/minusmc/ravenb4/module/modules/render/Gui.kt
+++ src/main/java/net/minusmc/ravenb4/module/modules/render/Gui.kt
@@ -6,7 +6,7 @@ import net.minusmc.ravenb4.module.ModuleCategory
 import net.minusmc.ravenb4.setting.impl.SliderSetting
 import org.lwjgl.input.Keyboard
 
-class Gui: Module("Gui", ModuleCategory.render, keyCode = Keyboard.KEY_RSHIFT) {
+class Gui: Module("ClickGui", ModuleCategory.render, keyCode = Keyboard.KEY_RSHIFT) {
 
     override fun onEnable() {
         toggleModule()
